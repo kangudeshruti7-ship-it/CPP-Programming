7277fe02bc81d5a531513367a0be1979a4b3e735
kangudeshruti7-ship-it <kangudeshruti7@gmail.com>
2026-07-17 12:52:07 +0530

    Add files via upload

---
+++ operator.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+int a=10,b=20;
+cout<<"addition="<<a+b<<endl;
+cout<<"sub="<<a-b<<endl;
+cout<<"multi="<<a*b<<endl;
+cout<<"div="<<a/b<<endl;
+cout<<"mod="<<a%b<<endl;
+}
